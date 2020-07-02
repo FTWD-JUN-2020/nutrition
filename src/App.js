ac6672a949b0ef2e9d22f872f1f706675c180cd7
rltrtl <rickytoledo@LAPTOP-3UVQIICU.localdomain>
2020-07-01 21:32:53 -0700

    finished lab

--- src/App.js
+++ src/App.js
@@ -1,76 +1,129 @@
 import React, { Component } from 'react';
+import './App.css';
 import foods from './foods.json';
+import FoodBox from './Foodbox';
 
 class App extends Component {
   state = {
-    showForm:true,
-    foods: foods,
-  }
+    foodList: foods,
+    showForm: false,
+    todaysFood: []
+  };
 
-  handleSubmit = e => {
-    e.preventDefault();
-    let newFood = { 
-      name: this.state.name, 
-      calories: this.state.calories, 
-      image:this.state.image
-    }
+  displayFood = () => {
+    let arr = this.state.foodList.map((food) => {
+      return (
+        <FoodBox
+          key={food.name}
+          name={food.name}
+          calories={food.calories}
+          image={food.image}
+          theClickedFoodProp={this.clickedFood}
+        />
+      );
+    });
+    return arr;
+  };
 
-
-    let copyOfFoods = [...this.state.foods]
-
-
-    copyOfFoods.unshift( newFood )
-
-    this.setState({   
-      showForm: false,
-      foods: copyOfFoods
+  displayTodaysFood = () => {
+    let arr = this.state.todaysFood.map((food) => {
+      return (
+        <li>{food.name} {food.calories}</li>
+      )
     })
-
-
-    // this.toggleForm() //Another option >> Call a method from another ones
+    return arr
   }
 
 
-  throwBatrang = () => {
-    console.log("POW!!!!")
-  }
-
-  handleChange = e => {
+  clickedFood = (food) => {
+    console.log('I clicked the food named:', food.name) 
+    let newTodaysFood = [...this.state.todaysFood] 
+    newTodaysFood.push(food) 
     this.setState({
-      [e.target.name]:e.target.value
+      todaysFood:newTodaysFood 
     })
   }
+
+
+
+
 
   toggleForm = () => {
     this.setState({
-      showForm: !this.state.showForm
-      //true: !true
-      //false: !false
+      showForm: !this.state.showForm,
+    });
+  };
+
+  addFood = (event) => {
+    event.preventDefault();
+    let newFoodObj = {  
+      name: this.state.name, 
+      calories: this.state.calories,
+      image: this.state.image,
+      quantity: 0,
+    };
+    
+    let foodListCopy = [...this.state.foodList]; 
+    foodListCopy.unshift(newFoodObj); 
+
+    this.setState({
+      foodList: foodListCopy, 
+      showForm: false,
+    });
+  };
+
+
+
+
+
+  handleChange = (event) => {
+    console.log(event.target.name);
+
+    this.setState({
+      [event.target.name]: event.target.value, 
+    });
+  };
+
+  searchList = (event) => {
+    
+    let filterFoods = foods.filter(eachFood=>{
+      return eachFood.name.toLowerCase().includes(event.target.value.toLowerCase())
     })
 
-    this.throwBatrang()
+    this.setState({
+      foodList:filterFoods
+    })
+
   }
+
 
   render() {
     return (
-      <div>
-          <h1>Log in</h1>
-          
-          <button onClick={this.toggleForm}>Toggle Form</button>
-
-          {this.state.showForm ? 
-
-          <form onSubmit={this.handleSubmit}>
-            <input onChange={this.handleChange} placeholder="name" name="name" type="text" />
-            <input onChange={this.handleChange} placeholder="calories" name="calories" type="text" />
-            <input onChange={this.handleChange} placeholder="image" name="image" type="text" />
-
+      <div className="App">
+        <input onChange={this.searchList} placeholder="search"/>
+        <br /> 
+        <button onClick={this.toggleForm}>Add Food</button>
+        {this.state.showForm ? (
+          <form onSubmit={this.addFood}>
+            <label>Food name:</label>
+            <input onChange={this.handleChange} type="text" name="name" />
+            <br />
+            <label>Calories:</label>
+            <input onChange={this.handleChange} type="number" name="calories" />
+            <br />
+            <label>Image URL:</label>
+            <input onChange={this.handleChange} type="text" name="image" />
+            <br />
             <input type="submit" />
           </form>
+        ) : (
+          ''
+        )}
 
-          : 'dont show form' }
-
-
+        <section class="foods">
+          <ul>{this.displayFood()}</ul>
+          <ul>{this.displayTodaysFood()}</ul>
+        </section>
       </div>
     );
   }
